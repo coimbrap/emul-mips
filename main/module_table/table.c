5e9dca1e6497c1adb2d8a3ca69b57cbec512bfb8
Pierre Coimbra <pierre.coimbra@libmail.eu>
2020-12-07 19:07:31 +0100

    Mise au propre du module de la table des symboles

--- main/module_table/table.c
+++ main/module_table/table.c
@@ -7,7 +7,8 @@
 #include "../module_hex/hex.h"
 #include "../module_registres/registres.h"
 
-
+/* prend en entrée la table des symboles, le nom du symbole et la valeur du PC */
+/* insère en tête le symbole dans la table des symboles */
 void insertionTete(symtable *tableorigine, char* nom, int valeur) {
 	symtable head=malloc(sizeof(symbol));
 	head->nom=malloc((strlen(nom)+1)*sizeof(char));
@@ -17,7 +18,10 @@ void insertionTete(symtable *tableorigine, char* nom, int valeur) {
 	*tableorigine=head;
 }
 
+/* prend en entrée la table des symboles, le nom du symbole et la valeur du PC */
+/* insère en queue le symbole dans la table des symboles */
 void insertionQueue(symtable *tableorigine, char* nom, int valeur) {
+	/* Si la liste chaînée est vide on insère en tête */
 	if((*tableorigine)==NULL) {
 		insertionTete(tableorigine,nom,valeur);
 	}
@@ -26,8 +30,10 @@ void insertionQueue(symtable *tableorigine, char* nom, int valeur) {
 		while(increment->suivant!=NULL) {
 			increment=increment->suivant;
 		}
+		/* On malloc le nouvel élément */
 		increment->suivant=malloc(sizeof(symbol));
 		increment=increment->suivant;
+		/* On malloc une place pour le nom et on le strcpy */
     increment->nom=malloc((strlen(nom)+1)*sizeof(char));
 		strcpy(increment->nom,nom);
 		increment->valeur=valeur;
@@ -35,6 +41,8 @@ void insertionQueue(symtable *tableorigine, char* nom, int valeur) {
 	}
 }
 
+/* prend en entrée la table des symboles */
+/* efface le premier élément de la liste */
 void supressionTete(symtable *tableorigine) {
 	if(*tableorigine!=NULL) {
 		symtable tablewithouthead=(*tableorigine)->suivant;
@@ -44,17 +52,21 @@ void supressionTete(symtable *tableorigine) {
 	}
 }
 
-void tableAffiche(symtable ptr){
-	if (NULL==ptr)
+/* prend en entrée la table des symboles */
+/* affiche l'ensemble de la table des symboles dans l'ordre */
+void tableAffiche(symtable symbols) {
+	if (NULL==symbols)
 		printf("Table vide!");
 	else
 		printf("Contenu de la table : ");
-	while (NULL!=ptr) {
-		printf("%s %i\n",ptr->nom,ptr->valeur);
-		ptr=ptr->suivant;
+	while (NULL!=symbols) {
+		printf("%s %i\n",symbols->nom,symbols->valeur);
+		symbols=symbols->suivant;
 	}
 }
 
+/* prend en entrée la table des symboles */
+/* libère l'ensemble de la table des symboles pour éviter les fuites mémoire */
 void libereTable(symtable *symbols) {
   symtable increment=NULL;
   if (*symbols!=NULL) {
@@ -67,6 +79,8 @@ void libereTable(symtable *symbols) {
   }
 }
 
+/* prend en entrée la table des symboles ainsi qu'un symbole à identifier */
+/* retourne le PC associé à ce symbole si non trouvé -1 */
 int foundSymbol(symtable *symbols, char* symbol) {
 	symtable increment=NULL;
 	int nonTrouvee=1,ret=-1;
@@ -89,26 +103,30 @@ int foundSymbol(symtable *symbols, char* symbol) {
 	return ret;
 }
 
-
+/* prend en entrée le fichier du programme, la structure des instruction, des registres et la table des symboles */
+/* on écrit par adresse */
+/* remplis la table des symboles et doit pour cela vérifié si les instructions sont correcte (parse + checksum) pour que la valeur du PC soit correcte */
 void fillSymbols(char *input, instruction **instructions, registre** registres, symtable *symbols) {
   int nbOpe=0,nbReg=0,nbImm=0,num=0,coeffOpe=0;
   char *p=NULL,*parse=NULL,*tampon=NULL,*s=NULL; /* tampon va être la zone de travail de strtok pour pouvoir utiliser strtok dans deux fonctions */
   int ret=1;
 	FILE *fin=NULL;
 	int pc=0xDDDC;
-	instruction *found=NULL;
+	instruction *instr=NULL;
 	size_t len=0;
 	int tailleTab=0;
 	fin=fopen(input, "r");
-    if (fin==NULL) {
-      printf("Erreur lors de l'ouverture du fichier '%s'\n",input);
-      exit(-1);
-    }
+  if (fin==NULL) {
+    printf("Erreur lors de l'ouverture du fichier '%s'\n",input);
+    exit(-1);
+  }
+	/* on parcourt tout le fichier ligne par ligne */
 	while(getline(&s,&len,fin)!=-1) {
 		ret=1;
 		nbReg=nbImm=nbOpe=num=coeffOpe=0;
 		parse=malloc(strlen(s)*sizeof(char));
 	  int state=nettoyageInstruction(s,&parse,&tailleTab,&nbOpe);
+		/* même principe que pour le parseur */
 	  if (state==0) {
 	    free(parse);
 	    ret=0;
@@ -123,8 +141,8 @@ void fillSymbols(char *input, instruction **instructions, registre** registres, 
 	    for (p=strtok_r(parse," ",&tampon);p!=NULL;p=strtok_r(NULL, " ",&tampon)) {
 	      /* Ecriture */
 	      if (num==0 && nbOpe>=0) {
-	        found=trouveOperation(instructions,p);
-	        if(found==NULL) {ret=0;}
+	        instr=trouveOperation(instructions,p);
+	        if(instr==NULL) {ret=0;}
 	      }
 	      /* Calcul de notre checksum */
 	      if (p[0]=='$') {
@@ -152,14 +170,12 @@ void fillSymbols(char *input, instruction **instructions, registre** registres, 
 	    }
 	    /* Comparaison du checksum avec le checksum théorique */
 	    if (ret!=0) {
-	      if((compareChecksum(nbReg,found->checksumReg,2)==0 || compareChecksum(nbImm,found->checksumImm,1)==0)){ret=0;};
+	      if((compareChecksum(nbReg,instr->checksumReg,2)==0 || compareChecksum(nbImm,instr->checksumImm,1)==0)){ret=0;};
 			}
 			if (ret==1) {pc+=4;}
 			free(parse);
 			free(p);
 	  }
 	}
-	tableAffiche(*symbols);
-	libereTable(symbols);
 	free(s);
 }
--- main/module_table/table.h
+++ main/module_table/table.h
@@ -9,14 +9,28 @@ typedef struct symbol {
 
 typedef symbol* symtable;
 
-//#include "../module_hex/hex.h"
-
+/* prend en entrée la table des symboles, le nom du symbole et la valeur du PC */
+/* insère en tête le symbole dans la table des symboles */
 void insertionTete(symtable *tableorigine, char* nom, int valeur);
+
+/* prend en entrée la table des symboles, le nom du symbole et la valeur du PC */
+/* insère en queue le symbole dans la table des symboles */
 void insertionQueue(symtable *tableorigine, char* nom, int valeur);
+
+/* prend en entrée la table des symboles */
+/* efface le premier élément de la liste */
 void supressionTete(symtable *tableorigine);
-void tableAffiche(symtable ptr);
+
+/* prend en entrée la table des symboles */
+/* affiche l'ensemble de la table des symboles dans l'ordre */
+void tableAffiche(symtable symbols);
+
+/* prend en entrée la table des symboles */
+/* libère l'ensemble de la table des symboles pour éviter les fuites mémoire */
 void libereTable(symtable *symbols);
+
+/* prend en entrée la table des symboles ainsi qu'un symbole à identifier */
+/* retourne le PC associé à ce symbole si non trouvé -1 */
 int foundSymbol(symtable *symbols, char* symbol);
-//void fillSymbols(char *input, instruction **instructions, registre** registres, symtable *symbols);
 
 #endif
